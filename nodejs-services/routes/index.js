713b2502e7fda99bafc789c743b1e47138aa9c39
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-04-07 00:10:43 +0530

    Update index.js

--- nodejs-services/routes/index.js
+++ nodejs-services/routes/index.js
@@ -8,7 +8,7 @@ module.exports = {
                 res.redirect('/');
             }
             res.render('index.ejs', {
-                title: Welcome to HELL | View Student's Details
+                title: Welcome to HELL | View Students Details
                 ,student_details: result
             });
         });
