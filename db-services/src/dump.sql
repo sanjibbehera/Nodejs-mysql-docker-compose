e2e5041b7ceaf365a3860ed62d719239ef9ac050
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-04-07 03:28:23 +0530

    correcting mysql code

--- db-services/src/dump.sql
+++ db-services/src/dump.sql
@@ -1,33 +1,18 @@
 CREATE DATABASE IF NOT EXISTS studentsdb;
+CREATE USER 'student'@'localhost' IDENTIFIED BY 'student123';
+GRANT ALL PRIVILEGES ON studentsdb.* TO 'student'@'localhost';
 
-CREATE TABLE IF NOT EXISTS `students` (
-    `student_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `student_name` VARCHAR(255) NOT NULL,
-    `student_class` VARCHAR(30) NOT NULL,
-)  ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+create table students ( student_id int unsigned not null auto_increment, student_name varchar(50) not null, student_class VARCHAR(30) not null, constraint pk_students primary key (student_id)) ENGINE=INNODB;
+insert into students (student_name, student_class) values ('RAJ', 'CLASS II');
+insert into students (student_name, student_class) values ('RAJA BABU', 'CLASS III');
+insert into students (student_name, student_class) values ('NANDU', 'CLASS IV');
+insert into students (student_name, student_class) values ('NANDU SABKA BANDHU', 'CLASS V');
 
-CREATE TABLE IF NOT EXISTS student_details (
-    `student_details_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `student_id` INT NOT NULL,
-    `student_first_name` VARCHAR(255) NOT NULL,
-    `student_last_name` VARCHAR(255) NOT NULL,
-    `student_class` VARCHAR(30) NOT NULL,
-    `student_class_section` VARCHAR(30) NOT NULL,
-    `student_school_name` VARCHAR(255) NOT NULL,
-    FOREIGN KEY (`student_id`)
-        REFERENCES students(`student_id`)
-        ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+create table student_details ( student_details_id int unsigned not null auto_increment, student_id int unsigned not null, student_first_name varchar(50) not null, student_last_name varchar(50) not null, student_class VARCHAR(30) not null, student_class_section varchar(30) not null, student_school_name varchar(100) not null, constraint pk_student_details primary key (student_details_id), FOREIGN KEY (student_id) REFERENCES students(student_id)) ENGINE=INNODB;
 
-
-INSERT INTO STUDENTS (student_name) VALUES ('RAJ', 'CLASS II');
-INSERT INTO STUDENTS (student_name) VALUES ('RAJA BABU', 'CLASS III');
-INSERT INTO STUDENTS (student_name) VALUES ('NANDU', 'CLASS IV');
-INSERT INTO STUDENTS (student_name) VALUES ('NANDU SABKA BANDHU', 'CLASS V');
-
-INSERT INTO STUDENT_DETAILS (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDNET_SCHOOL_NAME) VALUES (1, 'CLASS II', 'RAJ', 'DDLJ', 'VIII', 'RAPCHIK SCHOOL');
-INSERT INTO STUDENT_DETAILS (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDNET_SCHOOL_NAME) VALUES (2, 'CLASS III', 'RAJA BABU', 'COOLIE NO 1', 'VII', 'RAPCHIK SCHOOL');
-INSERT INTO STUDENT_DETAILS (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDNET_SCHOOL_NAME) VALUES (3, 'CLASS IV', 'NANDU', 'COOLIE NO 1', 'VI', 'RAPCHIK SCHOOL');
-INSERT INTO STUDENT_DETAILS (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDNET_SCHOOL_NAME) VALUES (4, 'CLASS I', 'NANDU SABKA BANDHU', 'RAJA BABU', 'V', 'RAPCHIK SCHOOL');
+insert into student_details (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDENT_SCHOOL_NAME) values (1, 'CLASS II', 'RAJ', 'DDLJ', 'VIII', 'RAPCHIK SCHOOL');
+insert into student_details (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDENT_SCHOOL_NAME) values (2, 'CLASS III', 'RAJA BABU', 'COOLIE NO 1', 'VII', 'RAPCHIK SCHOOL');
+insert into student_details (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDENT_SCHOOL_NAME) values (3, 'CLASS IV', 'NANDU', 'COOLIE NO 1', 'VI', 'RAPCHIK SCHOOL');
+insert into student_details (STUDENT_ID, STUDENT_CLASS, STUDENT_FIRST_NAME, STUDENT_LAST_NAME, STUDENT_CLASS_SECTION, STUDENT_SCHOOL_NAME) values (4, 'CLASS I', 'NANDU SABKA BANDHU', 'RAJA BABU', 'V', 'RAPCHIK SCHOOL');
 
 COMMIT;
