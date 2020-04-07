763812239f7f3910e94fe1c5e04d4a1912e2ae12
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-04-07 22:13:42 +0530

    Table Restructuring.

--- db-services/src/dump.sql
+++ db-services/src/dump.sql
@@ -2,11 +2,11 @@ CREATE DATABASE IF NOT EXISTS studentsdb;
 CREATE USER 'student'@'localhost' IDENTIFIED BY 'student123';
 GRANT ALL PRIVILEGES ON studentsdb.* TO 'student'@'localhost';
 
-create table students ( student_id int unsigned not null auto_increment, student_name varchar(50) not null, student_class VARCHAR(30) not null, constraint pk_students primary key (student_id)) ENGINE=INNODB;
-insert into students (student_name, student_class) values ('RAJ', 'CLASS II');
-insert into students (student_name, student_class) values ('RAJA BABU', 'CLASS III');
-insert into students (student_name, student_class) values ('NANDU', 'CLASS IV');
-insert into students (student_name, student_class) values ('NANDU SABKA BANDHU', 'CLASS V');
+create table students ( student_id int unsigned not null auto_increment, student_first_name varchar(50) not null, student_last_name varchar(50) not null, student_class VARCHAR(30) not null, constraint pk_students primary key (student_id)) ENGINE=INNODB;
+insert into students (student_first_name, student_class) values ('RAJ', 'DDLJ', 'CLASS II');
+insert into students (student_first_name, student_class) values ('RAJA BABU', 'COOLIE NO 1', 'CLASS III');
+insert into students (student_first_name, student_class) values ('NANDU', 'COOLIE NO 1', 'CLASS IV');
+insert into students (student_first_name, student_class) values ('NANDU SABKA BANDHU', 'RAJA BABU', 'CLASS V');
 
 create table student_details ( student_details_id int unsigned not null auto_increment, student_id int unsigned not null, student_first_name varchar(50) not null, student_last_name varchar(50) not null, student_class VARCHAR(30) not null, student_class_section varchar(30) not null, student_school_name varchar(100) not null, constraint pk_student_details primary key (student_details_id), FOREIGN KEY (student_id) REFERENCES students(student_id)) ENGINE=INNODB;
 
