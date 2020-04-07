4783594500a116dfa6f234e571ec22a44c5688e2
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-04-07 19:07:12 +0530

    typo fix

--- nodejs-services/routes/index.js
+++ nodejs-services/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var connection  = require('./config/dbConfig');
+var connection  = require('../config/dbConfig');
 
 
 /* GET home page. */
